b06f35832ba4c736b2ecb2d9617fa9cc0ca8fd9d
Nkosana <41477334+1439120@users.noreply.github.com>
2021-07-04 21:37:52 +0200

    Update db.js

--- public/scripts/db.js
+++ public/scripts/db.js
@@ -3,10 +3,10 @@ const mssql = require('mssql')
 // Make sure this is private to this module
 const config = {
   server: 'sgm-server-2.database.windows.net',
-  database: process.env.DB_NAME || 'StudyGroupDB',
+  database: 'StudyGroupDB',
   // Put login details in env. variables for security
-  user: process.env.DB_USER || 'StudyGroupManager',
-  password: process.env.DB_PASS || '#1Study-Group',
+  user: 'StudyGroupManager',
+  password: '#1Study-Group',
   port: 1433,
   // Required for Azure
   options: {
